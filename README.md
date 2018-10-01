77bfce1f5dae4caafc14c17c1b8a2cbfe7da7288
Jon Knight <jon.knight@forgerock.com>
2018-10-01 17:48:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<!--
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2017 ForgeRock AS.
+-->
+<b>Have I Been Pwned Password Authentication Node</b>
+<br/>
+An authentication decision node which checks the supplied password against the Have I Been Pwned database of passwords previously exposed in data breaches (https://haveibeenpwned.com/Passwords).
+<br/>
+The Pwned Passwords service was created in August 2017 after NIST released guidance specifically recommending that user-provided passwords be checked against existing data breaches. In February 2018, version 2 of the service was released with more than half a billion passwords, each now also with a count of how many times they'd been seen exposed. A version 3 release in July 2018 contributed a further 15.6M passwords to bring the total to 517M.
+<br/>
+<br/>
+<b>Installation</b>
+<br/>
+Copy the .jar file from the ../target directory into the ../web-container/webapps/openam/WEB-INF/lib directory where AM is deployed.  Restart the web container to pick up the new node.  The node will then appear in the authentication trees components palette.
+<br/>
+<br/>
+<b>Usage</b>
+<br/>
+The node configuration requires the name of the shared state variable (or transient shared state) containing the password to validate, and a threshold indicating the maximum number of recorded breaches the password is associated with before it is rejected. A value of 0 will reject any password which has been recorded in any breach.
+<br/>
+The node provides 2 outcomes: true, indicating that the password is associated with a known breach, or false, indicating that no known breaches.
+<br/>
+<br/>
+![ScreenShot](./haveibeenpwned.png)
+<br/>
+<br/>
+<b>Disclaimer</b>
+The sample code described herein is provided on an "as is" basis, without warranty of any kind, to the fullest extent permitted by law. ForgeRock does not warrant or guarantee the individual success developers may have in implementing the sample code on their development platforms or in production configurations.
+
+ForgeRock does not warrant, guarantee or make any representations regarding the use, results of use, accuracy, timeliness or completeness of any data or information relating to the sample code. ForgeRock disclaims all warranties, expressed or implied, and in particular, disclaims all warranties of merchantability, and warranties related to the code, or any service or software related thereto.
+
+ForgeRock shall not be liable for any direct, indirect or consequential damages or costs of any type arising out of any action taken by you or others related to the sample code.
---
+++ src/main/java/org/forgerock/openam/auth/nodes/HaveIBeenPwnedPasswordNode.java
@@ -0,0 +1,141 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2017 ForgeRock AS.
+ */
+/**
+ * jon.knight@forgerock.com
+ *
+ * A node that returns true if the user's email address is recorded as breached by the HaveIBeenPwned website (http://haveibeenpwned.com)
+ * or false if no breach has been recorded
+ */
+
+
+package org.forgerock.openam.auth.nodes;
+
+import com.google.inject.assistedinject.Assisted;
+import com.sun.identity.shared.debug.Debug;
+import org.forgerock.openam.auth.node.api.*;
+import org.forgerock.openam.core.CoreWrapper;
+import javax.inject.Inject;
+import org.forgerock.openam.annotations.sm.Attribute;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
+
+
+@Node.Metadata(outcomeProvider = AbstractDecisionNode.OutcomeProvider.class,
+        configClass = HaveIBeenPwnedPasswordNode.Config.class)
+public class HaveIBeenPwnedPasswordNode extends AbstractDecisionNode {
+
+
+    /**
+     * Configuration for the node.
+     */
+    public interface Config {
+        @Attribute(order = 100)
+        default String password() { return "password"; }
+        @Attribute(order = 200)
+        default int threshold() { return 0; }
+    }
+
+
+    private final Config config;
+    private final CoreWrapper coreWrapper;
+    private final static String DEBUG_FILE = "HaveIBeenPwnedPasswordNode";
+    protected Debug debug = Debug.getInstance(DEBUG_FILE);
+
+    /**
+     * Guice constructor.
+     * @param config The node configuration.
+     * @throws NodeProcessException If there is an error reading the configuration.
+     */
+    @Inject
+    public HaveIBeenPwnedPasswordNode(@Assisted Config config, CoreWrapper coreWrapper) throws NodeProcessException {
+        this.config = config;
+        this.coreWrapper = coreWrapper;
+    }
+
+    @Override
+    public Action process(TreeContext context) throws NodeProcessException {
+
+        String pass = context.transientState.get(config.password()).asString();
+        if (pass == null) pass = context.sharedState.get(config.password()).asString();
+        if (pass == null) {
+            debug.error("[" + DEBUG_FILE + "]: " + "couldn't find password variable in transient or shared state: " + config.password());
+            // Assume compromised state
+            return goTo(true).build();
+        }
+        String hex = null;
+        try {
+            MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+            sha1.update(pass.getBytes("UTF-8"));
+            hex = Hex.encodeHexString(sha1.digest());
+            debug.message("[" + DEBUG_FILE + "]: " + "SHA1 hash of password: " + hex);
+        } catch (Exception e) {
+            debug.error("[" + DEBUG_FILE + "]: " + "Hash failed");
+            // Assume compromised state
+            return goTo(true).build();
+        }
+        return goTo(haveIBeenPwnedPassword(hex)).build();
+    }
+
+
+    private boolean haveIBeenPwnedPassword(String hex) {
+        hex = hex.toUpperCase();
+        String prefix = hex.substring(0,5);
+        try {
+            URL url = new URL("https://api.pwnedpasswords.com/range/" + prefix);
+            debug.message("[" + DEBUG_FILE + "]: url = " + url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.33 Safari/537.36");
+
+            if (conn.getResponseCode() != 200) {
+                debug.error("[" + DEBUG_FILE + "]: HTTP failed, response code:" + conn.getResponseCode());
+                throw new RuntimeException("[" + DEBUG_FILE + "]: HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output;
+            while ((output = br.readLine()) != null) {
+                if (prefix.concat(output).startsWith(hex)) {
+                    debug.message("[" + DEBUG_FILE + "]: found matching password: " + output);
+                    // Compromised password match
+                    String[] parts;
+                    parts = output.split(":");
+                    int breaches = Integer.parseInt(parts[1]);
+                    // If password matched and number of hits is greater than threshold then compromised is true
+                    if (breaches > config.threshold()) return true;
+                }
+            }
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // No matching password found
+        debug.message("[" + DEBUG_FILE + "]: " + "Password is safe.");
+        return false;
+    }
+
+}
